e921d9895612d1d7cfe3ea11225e8afb58406019
kut7728 <kut7728@hanyang.ac.kr>
2025-02-26 09:39:38 +0900

    [250226] 야곰 UIKit 강의 완료 & 주석달기

--- UpDownGame/CreditViewController.swift
+++ UpDownGame/CreditViewController.swift
@@ -11,6 +11,8 @@ class CreditViewController: UIViewController {
 
     
     @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        
+        // 이전 화면으로 돌아가는 코드, 애니메이션 있고, 돌아간 후에 따로 할일은 없으니 completion: nil
         dismiss(animated: true, completion: nil)
     }
 
--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -7,40 +7,54 @@
 
 import UIKit
 
+// 스토리보드에 추가된 씬을 관리하는 뷰 컨트롤러 파일
 class ViewController: UIViewController {
     
+    // 컴이 생각하는 숫자
     var randomValue: Int = 0
+    // 사용자의 시도 횟수
     var tryCount: Int = 0
     
+    // @IBOutlet 스토리보드속 컴포넌트를 변수화
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var tryCountLabel: UILabel!
     @IBOutlet weak var sliderValueLabel: UILabel!
     @IBOutlet weak var minimumValueLabel: UILabel!
     @IBOutlet weak var maximumValueLabel: UILabel!
 
+    // 뷰 생명주기 중 viewDidLoad에 실행될 코드
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 슬라이더의 손잡이에 커스텀 이미지 넣기, .normal: 평상시를 포함한 모든 상황에서 사용
         slider.setThumbImage(UIImage(named: "slider_thumb"), for: .normal)
         
         reset()
     }
     
+    // @IBAction 스토리보드속 컴포넌트 상호작용시에 실행될 함수 선언
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print(sender.value)
-        sliderValueLabel.text = String(Int(sender.value))
+        print(sender.value)  // 슬라이더의 현재 값을 로그에 프린트
+        sliderValueLabel.text = String(Int(sender.value))  // 스토리보드상의 sliderValueLabel에 슬라이더 값을 표시
     }
     
+    // 알림표시 함수
     func showAlert(message: String) {
+        // 제목은 없고, 본문만 있고, 스타일은 알림형식의 alertController 객체
         let alert = UIAlertController(title: nil,
                                       message: message,
                                       preferredStyle: .alert)
+        
+        // OK라고 표시되는 평범한 버튼 스타일의 alertAction 객체, 누르면 reset()
         let okAction = UIAlertAction(title: "OK",
                                      style: .default) { _ in
             self.reset()
         }
         
+        // alert 객체에 action객체 추가
         alert.addAction(okAction)
+        
+        // alert객체 표시 방법, 애니메이션 존재, 완료된후 할건 딱히 없음
         present(alert,
                 animated: true,
                 completion: nil)
